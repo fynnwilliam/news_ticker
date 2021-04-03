2336b4f5b54ee674d604931a14bdf7379919957d
fynnwilliam <fynnwilliam@gmail.com>
2021-04-02 23:41:52 -0400

    accepting custom text

--- main.cpp
+++ main.cpp
@@ -9,16 +9,20 @@
 int main()
 {
 	using namespace std::chrono_literals;
-	
-	std::system("clear");
 
-	std::string banner {"welcome to the morning show..."};
-	banner.insert(banner.begin(), 5, ' ');
+	std::string slide;
+
+	std::cout << "please enter a message for the slide: ";
+	std::getline(std::cin, slide);
+	
+	slide.insert(slide.begin(), 5, ' ');
+
+	std::system("clear");
 
 	for(int i = 0; i < 10000; i++)
 	{
-		std::rotate(banner.begin(), banner.begin() + 1, banner.end());
-		std::copy(banner.begin(), banner.end(), std::ostreambuf_iterator<char>(std::cout));
+		std::rotate(slide.begin(), slide.begin() + 1, slide.end());
+		std::copy(slide.begin(), slide.end(), std::ostreambuf_iterator<char>(std::cout));
 		std::cout << std::flush;
 		std::this_thread::sleep_for(1000ms);
 		std::system("clear");
