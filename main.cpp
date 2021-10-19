c6bac069f0d9904808caa873d8555837c8575359
fynnwilliam <fynnwilliam@gmail.com>
2021-10-19 05:08:30 -0400

    implemented a rotate and extract_message function

--- main.cpp
+++ main.cpp
@@ -1,27 +1,43 @@
-#include <algorithm>
 #include <iostream>
+#include <iomanip>
 #include <thread>
 #include <chrono>
 #include <string>
+
+std::string extract_message(int argc, char** argv)
+{
+    std::string s;
+    
+    for (int i{1}; i < argc; ++i)
+        s.append(argv[i]).append(" ");
+    
+    return s;    
+}
 
 void clear_line()
 {
     std::cout << "\u001b[2K";
 }
 
+std::string rotate(std::string const& s, int count)
+{
+    return count > s.size() && s.size() < 70
+           ?
+           s.substr(0, count).append(count - s.size(), ' ')
+           :
+           s.substr(0, count);
+}
+
 int main(int argc, char** argv)
 {
 	using namespace std::chrono_literals;
 
-	std::string slide = argv[1];
-	
-	slide.insert(slide.begin(), 5, ' ');
+	std::string slide = extract_message(argc, argv);
 
-	for(int i = 0; i < 5; ++i)
+	for(int i = 0; i < 100; ++i)
 	{
-		std::rotate(slide.begin(), slide.begin() + 1, slide.end());
-		std::cout << slide + '\r' << std::flush;
-		std::this_thread::sleep_for(1000ms);
+		std::cout << std::setw(70) << rotate(slide, i) + '\r' << std::flush;
+		std::this_thread::sleep_for(100ms);
 	}
 	
 	clear_line();
