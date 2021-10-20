4d728b0b4c8609cc9b390764f9878ce9e948bbf8
fynnwilliam <fynnwilliam@gmail.com>
2021-10-20 15:27:47 -0400

    added some tests...

---
+++ tests/slide_tests.cpp
@@ -0,0 +1,24 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+#include "slide.h"
+
+TEST_CASE("get_slide() should return an lvalue of slide object")
+{
+    
+}
+
+TEST_CASE("message() should return a const lvalue of the class data member, message_")
+{
+    
+}
+
+TEST_CASE("message(int, char**) should assign the pointed to object to message_")
+{
+    
+}
+
+TEST_CASE("message(std::string) should move the value parsed to it to message_")
+{
+    
+} 
