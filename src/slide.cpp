5458ca134a39c44480ea433a28beaee924cb2f9b
fynnwilliam <fynnwilliam@gmail.com>
2021-10-20 00:28:36 -0400

    updated rotate function

--- src/slide.cpp
+++ src/slide.cpp
@@ -26,19 +26,19 @@ void slide::message(std::string s)
 std::string slide::rotate(std::size_t count) const
 {
     std::string const& s = message_;
-    return count > 70 && s.size() >= 70 && s.substr(count - 70, 70).size() < 70
+    return count > 69 && s.size() >= 70 && s.substr(count - 69, 70).size() < 70
            ?
-           s.substr(count - 70, 70).append(70 - s.substr(count - 70, 70).size(),' ')
+           s.substr(count - 69, 69).append(70 - s.substr(count - 69, 70).size(),' ')
            :
-           count > 70 && s.size() >= 70
+           count > 69 && s.size() >= 70
            ?
-           s.substr(count - 70, 70)
+           s.substr(count - 69, 69)
            :
-           count > 70 && s.size() < 70
+           count > 69 && s.size() < 70
            ?
-           s.substr(count - 70).append(count - s.size(), ' ')
+           s.substr(count - 69).append(count - s.size(), ' ')
            :
-           count > s.size() && s.size() < 70
+           count > s.size() - 1 && s.size() < 70
            ?
            s.substr(0, count).append(count - s.size(), ' ')
            :
@@ -57,7 +57,7 @@ void slide::display() const
     for(std::size_t i = 0; i < message_.size() + 70; ++i)
 	{
 		std::cout << std::setw(70) << rotate(i) + '\r' << std::flush;
-		std::this_thread::sleep_for(100ms);
+		std::this_thread::sleep_for(300ms);
 	}
 	
 	clear_line();
