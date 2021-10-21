be48c50a018b5e3e87cf88e785bf304085acb49b
fynnwilliam <fynnwilliam@gmail.com>
2021-10-20 22:23:54 -0400

    testing member function, message()

--- tests/slide_tests.cpp
+++ tests/slide_tests.cpp
@@ -16,7 +16,26 @@ TEST_CASE("get_slide() should always return an lvalue of  the same slide object"
 
 TEST_CASE("message() should return a const lvalue of the class data member, message_")
 {
+    slide& s = slide::get_slide();
     
+    std::string const& first_msg = s.message();
+    std::string const& second_msg = s.message();
+    
+    SECTION("s.message() should return an empty string")
+    {
+        REQUIRE(s.message().size() == 0);
+    }
+    
+    SECTION("the size of first_msg should be equal to the size of second_msg")
+    {
+        REQUIRE(first_msg.size() == 0);
+        REQUIRE(second_msg.size() == 0);
+    }
+    
+    SECTION("address of first_msg should be equal to the address of second_msg")
+    {
+        REQUIRE(&first_msg == &second_msg);
+    }
 }
 
 TEST_CASE("message(int, char**) should assign the pointed to object to message_")
