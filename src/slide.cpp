6138d2654bc9b571ea49b1ab428b6ef644af22e9
fynnwilliam <fynnwilliam@gmail.com>
2025-05-05 21:11:28 +0100

    refactor: rename max -> max_counter

--- src/slide.cpp
+++ src/slide.cpp
@@ -43,7 +43,8 @@ void slide::reset_counter() noexcept { counter_ = 0uz; }
 void slide::display() {
   using namespace std::chrono_literals;
 
-  for (auto max = message_.size() + screen_width_; counter_ < max; ++counter_) {
+  auto max_counter = message_.size() + screen_width_;
+  for (; counter_ < max_counter; ++counter_) {
     std::cout << std::setw(screen_width_) << rotate() + '\r' << std::flush;
     std::this_thread::sleep_for(150ms);
   }
