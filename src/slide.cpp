8b5baada8a9f94687cb11656658e98e11a3ef039
fynnwilliam <fynnwilliam@gmail.com>
2025-05-06 01:38:24 +0100

    refactor: remove unsued functions

--- src/slide.cpp
+++ src/slide.cpp
@@ -1,32 +1,7 @@
-#include "slide.h"
-
-#include <chrono>
+#include <string>
 #include <iomanip>
 #include <iostream>
 #include <thread>
-
-slide& slide::get_slide() noexcept {
-  static slide s{};
-  return s;
-}
-
-void slide::message(int argc, char** argv) noexcept {
-  for (int i{1}; i < argc; ++i)
-    message_.append(argv[i]).append(" ");
-
-  if (message_.size())
-    message_.pop_back();
-}
-
-void slide::message(std::string s) noexcept { message() = std::move(s); }
-
-std::string& slide::message() noexcept { return message_; }
-
-void slide::counter(std::size_t c) noexcept { counter_ = c; }
-
-std::size_t slide::counter() const noexcept { return counter_; }
-
-void slide::reset_counter() noexcept { counter_ = 0uz; }
 
 const auto slide = [](std::string msg, const size_t width = 75) {
   using namespace std::chrono_literals;
@@ -40,5 +15,3 @@ const auto slide = [](std::string msg, const size_t width = 75) {
     std::this_thread::sleep_for(150ms);
   }
 };
-
-void slide::display() { ::slide(message_, screen_width_); }
