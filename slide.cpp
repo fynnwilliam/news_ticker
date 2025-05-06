edb1d836a3b1b120fabaff6dec2c54702e54e363
fynnwilliam <fynnwilliam@gmail.com>
2025-05-06 03:09:55 +0100

    refactor: rename param msg -> note

--- slide.cpp
+++ slide.cpp
@@ -3,12 +3,12 @@
 #include <string>
 #include <thread>
 
-const auto slide = [](std::string msg, const size_t width = 75) {
+const auto slide = [](std::string note, const size_t width = 75) {
   using namespace std::chrono_literals;
 
-  msg.append(width, ' ');
-  const char* msg_ptr = msg.data();
-  for (auto size = msg.size(), index = 0zu; index < size; ++index) {
+  note.append(width, ' ');
+  const char* msg_ptr = note.data();
+  for (auto size = note.size(), index = 0zu; index < size; ++index) {
     auto s = index >= width ? std::string_view{++msg_ptr, width}
                             : std::string_view{msg_ptr, index};
     std::cout << std::setw(width) << s << '\r' << std::flush;
