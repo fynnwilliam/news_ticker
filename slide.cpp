4dc59e4811f45d02f8826cab3c693ad7c4e6e84f
fynnwilliam <fynnwilliam@gmail.com>
2025-05-07 16:57:33 +0100

    feat: add slide_until
    it accepts a condition telling when to end sliding

--- slide.cpp
+++ slide.cpp
@@ -16,6 +16,25 @@ const auto slide = [](std::string note, const size_t width = 75) {
   }
 };
 
+auto conditon = [i = 0] mutable { return i ^= 1 == 1; };
+template <typename Func = decltype(conditon)>
+auto slide_until(
+    std::string note, const size_t width = 75, Func keep_sliding = conditon
+) {
+  using namespace std::chrono_literals;
+
+  note.append(width, ' ');
+  while (keep_sliding()) {
+    const char* n_ptr = note.data();
+    for (auto size = note.size(), index = 0zu; index < size; ++index) {
+      auto s = index >= width ? std::string_view{++n_ptr, width}
+                              : std::string_view{n_ptr, index};
+      std::cout << std::setw(width) << s << '\r' << std::flush;
+      std::this_thread::sleep_for(150ms);
+    }
+  }
+};
+
 const auto message = [](int argc, char** argv) {
   std::string message;
   for (int index{1}; index < argc; ++index)
@@ -27,5 +46,5 @@ const auto message = [](int argc, char** argv) {
 int main(int argc, char** argv) {
   auto note = argc > 1 ? message(argc, argv)
                        : "hi, welcome to C++ London | CppCon | ACCU | CppOnSea";
-  slide(note);
+  slide_until(note);
 }
